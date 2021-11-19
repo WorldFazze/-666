d92ec60bdaa00365a38a10d2ab83a70a061121f9
Арсений Бровкин <arseniybrovkin1.steam@yandex.ru>
2021-11-19 20:22:48 +0700

    Добавьте файлы проекта.

---
+++ лаба 666/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace GraphicsForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(-12, 63);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(582, 389);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(321, 458);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 24);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Пуск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(453, 458);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Закрыть";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(73, 22);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            this.numericUpDown1.Leave += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Скорость";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(422, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(370, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Таймер";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(199, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "label4";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(540, 493);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Бильярд";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ лаба 666/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace GraphicsForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label2.Text = "";
+            label3.Text = "Таймер";
+            label4.Text = "";
+            timer1.Start();
+
+        }
+
+        private int v;
+        private int tick;
+        int r;
+        int m;
+        int b;
+        private bool button1WasClicked = false;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+            if (button1WasClicked == false)
+            {
+                label4.Text = "Запустите сначала программу";
+                numericUpDown1.Value = 0;
+            }
+            else
+            {
+
+                int k = 0;
+                Graphics g = pictureBox1.CreateGraphics();
+                v = (int)numericUpDown1.Value;
+                Random random = new Random();
+
+                g.DrawRectangle(Pens.White, 0, 0, 547, 389);
+                double x = 255;
+                double y = 255;
+                double a = random.Next(0, 361);
+
+                double vx = v * Math.Cos(a);
+                double vy = v * Math.Sin(a);
+
+                while (true)
+                {
+                    v = (int)numericUpDown1.Value;
+                    x += vx;
+                    y += vy;
+                    SolidBrush brush = new SolidBrush(Color.FromArgb(r, m, b));
+                    if (tick < 10)
+                    {
+                        r = 0;
+                        m = 73;
+                        b = 255;
+                    }
+
+                    if (tick % 10 == 0)
+                    {
+                        if (k == 0)
+                        {
+
+                            r = random.Next(0, 255);
+                            m = random.Next(0, 255);
+                            b = random.Next(0, 255);
+                            k++;
+                        }
+                    }
+                    if (tick % 11 == 0)
+                    {
+                        k = 0;
+                    }
+
+                    if (x < 10) vx = -vx;
+                    if (x > 533) vx = -vx;
+                    if (y < 0) vy = -vy;
+                    if (y > 370) vy = -vy;
+
+                    pictureBox1.Image = null;
+                    g.FillEllipse(brush, Convert.ToInt32(Math.Round(x)), Convert.ToInt32(Math.Round(y)), 20, 20);
+                    Thread.Sleep(10);
+                    Application.DoEvents();
+                }
+            }
+
+
+        }
+
+
+            private void pictureBox1_Paint(object sender, PaintEventArgs e)
+            {
+
+
+
+            }
+
+            private void timer1_Tick(object sender, EventArgs e)
+            {
+                tick++;
+                label2.Text = tick.ToString();
+
+
+            }
+
+            private void button1_Click(object sender, EventArgs e)
+            {
+            label4.Text = "";
+            button1WasClicked = true;
+            Graphics g = pictureBox1.CreateGraphics();
+            Random random = new Random();
+
+
+            SolidBrush brush = new SolidBrush(Color.Blue);
+
+
+            g.DrawRectangle(Pens.White, 0, 0, 547, 389);
+            double x = 255;
+            double y = 255;
+            g.FillEllipse(brush, Convert.ToInt32(Math.Round(x)), Convert.ToInt32(Math.Round(y)), 20, 20);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+            {
+                Environment.Exit(0);
+            }
+
+
+
+            private void label2_Click(object sender, EventArgs e)
+            {
+
+            }
+
+            private void label3_Click(object sender, EventArgs e)
+            {
+
+            }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+    }
+    } 
